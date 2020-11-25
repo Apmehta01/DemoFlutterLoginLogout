b7fdea4a0d2c5da16965b53096b27eec7d3c208d
Apmehta01 <ap_90336>
2020-11-25 12:19:50 +0530

    1)Apply pagination

--- lib/app_screens/randomUserList.dart
+++ lib/app_screens/randomUserList.dart
@@ -3,7 +3,10 @@ import 'package:demoflutterloginlogout/model/randomAPIModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 
 class RandomUserList extends StatefulWidget {
   final String title;
@@ -20,11 +23,17 @@ class _RandomUserListState extends State<RandomUserList> {
   List<RandomAPIModel> searchResultList = List<RandomAPIModel>();
   Future _future;
   bool noMatchFound = false;
+  List userResponse = new List();
+  ProgressDialog progressdialog;
+
+  //Pagination
+  static int page = 1;
+  final dio = new Dio();
 
   @override
   void initState() {
+    _future = getRandomUser(context, page);
     super.initState();
-    _future = getRandomUser(context);
   }
 
   @override
@@ -38,41 +47,68 @@ class _RandomUserListState extends State<RandomUserList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          tooltip: "Reload",
-          child: Icon(Icons.refresh, color: Colors.white, size: 25.0),
-          onPressed: () {
-            setState(() {
-              _future = getRandomUser(context);
-              getFutureBuilder(_future);
-            });
-          },
+      floatingActionButton: Padding(
+        padding: EdgeInsets.zero,
+        child: Container(
+          height: 45.0,
+          width: 45.0,
+          child: FloatingActionButton(
+            tooltip: "Reload",
+            child: Icon(Icons.refresh, color: Colors.white, size: 25.0),
+            onPressed: () {
+              setState(() {
+                page = 0;
+                _future = getRandomUser(context, page);
+                getFutureBuilder(_future);
+              });
+            },
+          ),
         ),
-        body: Center(
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onPanDown: (_) {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Center(
           child: getFutureBuilder(_future),
-        ));
+        ),
+      ),
+      resizeToAvoidBottomPadding: false,
+    );
   }
 
   /**
    * Name : getRandomUser
    * <br> Purpose : This method is for getting data from server.
    */
-  Future<List<RandomAPIModel>> getRandomUser(BuildContext context) async {
-    final jobListAPIURL = 'https://randomuser.me/api/?results=10';
-    final response = await http.get(jobListAPIURL);
+  Future<List<RandomAPIModel>> getRandomUser(
+      BuildContext context, int pageNumber) async {
+    showProgressDialog();
+    // final jobListAPIURL = 'https://randomuser.me/api/?results=10';
+    final jobListAPIURL = "https://randomuser.me/api/?page=" +
+        pageNumber.toString() +
+        "&results=11&seed=abc";
+    // final response = await http.get(jobListAPIURL);
+    final response = await dio.get(jobListAPIURL);
+    debugPrint('URL:>>>>>>> ' + jobListAPIURL);
     if (response.statusCode == 200) {
-      List userResponse = json.decode(response.body)['results'];
+      for (int i = 0; i < response.data['results'].length; i++) {
+        userResponse.add(response.data['results'][i]);
+        debugPrint('DATA:>>>>>>> ' + response.data['results'][i].toString());
+      }
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Loading Data Sucess.'),
+        duration: const Duration(seconds: 2),
+        content: Text('Adding data Sucess.'),
       ));
       return userResponse
           .map((randomapimodel) => RandomAPIModel.fromJson(randomapimodel))
           .toList();
     } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Loading Data Failed!!'),
+        content: Text('Error while adding the data!!'),
       ));
-      throw Exception('Failed to load data from API');
+      throw Exception('Failed to load data from API!!');
     }
   }
 
@@ -85,6 +121,7 @@ class _RandomUserListState extends State<RandomUserList> {
         future: _future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            print("SNAP SIZE:>>>>>>>" + snapshot.data.length.toString());
             randomUserList = List.from(snapshot.data);
             return setUpRandomUserListWithSearch(randomUserList);
           } else if (snapshot.hasError) {
@@ -99,6 +136,7 @@ class _RandomUserListState extends State<RandomUserList> {
    * <br> Purpose : This method will sets up listview.
    */
   Widget setUpRandomUserListWithSearch(List<RandomAPIModel> randomUserList) {
+    debugPrint('SIZE:>>>>>>> ' + randomUserList.length.toString());
     return Container(
       margin: EdgeInsets.only(left: 10.0, right: 10.0),
       child: Column(
@@ -108,7 +146,7 @@ class _RandomUserListState extends State<RandomUserList> {
             child: TextField(
               controller: _textController,
               decoration: InputDecoration(
-                hintText: 'Search Name Here...',
+                hintText: 'Search name here...',
               ),
               onChanged: (value) {
                 setState(() {
@@ -140,35 +178,69 @@ class _RandomUserListState extends State<RandomUserList> {
           ? ListView.builder(
               itemCount: searchResultList.length,
               itemBuilder: (context, index) {
+                String name = /*index.toString()+". "+*/ searchResultList[index]
+                        .name
+                        .first +
+                    " " +
+                    searchResultList[index].name.last;
                 return setUpTitle(
                     index,
-                    searchResultList[index].name.first + " " +
-                    searchResultList[index].name.last,
+                    name,
                     searchResultList[index].gender,
                     searchResultList[index].email,
                     searchResultList[index].phone,
                     searchResultList[index].location.city,
                     searchResultList[index].location.country,
-                    searchResultList[index].picture.medium,
+                    searchResultList[index].picture.large,
                     searchResultList[index].dob.age.toString());
-              },
-            )
+              })
           : ListView.builder(
-              itemCount: randomUserList.length,
+              itemCount: randomUserList.length + 1,
               itemBuilder: (context, index) {
-                return setUpTitle(
-                    index,
-                    randomUserList[index].name.first + " " +
-                    randomUserList[index].name.last,
-                    randomUserList[index].gender,
-                    randomUserList[index].email,
-                    randomUserList[index].phone,
-                    randomUserList[index].location.city,
-                    randomUserList[index].location.country,
-                    randomUserList[index].picture.medium,
-                    randomUserList[index].dob.age.toString());
-              },
-            );
+                if (index == randomUserList.length) {
+                  return Center(
+                      child: Container(
+                    margin: EdgeInsets.only(bottom: 20.0, top: 10.0),
+                    width: 170.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.lightBlueAccent)),
+                      color: Colors.lightBlueAccent,
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Load More",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          page++;
+                          _future = getRandomUser(context, page);
+                        });
+                      },
+                    ),
+                  ));
+                } else {
+                  String name = index.toString() +
+                      ". " +
+                      randomUserList[index].name.first +
+                      " " +
+                      randomUserList[index].name.last;
+                  print("LIST COME:>>>>>>>");
+                  return setUpTitle(
+                      index,
+                      name,
+                      randomUserList[index].gender,
+                      randomUserList[index].email,
+                      randomUserList[index].phone,
+                      randomUserList[index].location.city,
+                      randomUserList[index].location.country,
+                      randomUserList[index].picture.large,
+                      randomUserList[index].dob.age.toString());
+                }
+              });
     }
   }
 
@@ -177,13 +249,14 @@ class _RandomUserListState extends State<RandomUserList> {
    * <br> Purpose : This method will sets up listtitle of listview.
    */
   Widget setUpTitle(int index, String name, String gender, String email,
-      String phone, String city, String country, String thumbnail, String age) {
+      String phone, String city, String country, String picture, String age) {
+    progressdialog.hide();
     return Card(
       elevation: 1.5,
       margin: EdgeInsets.only(top: 7.0, bottom: 7.0),
       child: ListTile(
           onTap: () {
-            openRandomUserInformationDialog(index);
+            openRandomUserInformationDialog(picture, name, email, phone, city);
           },
           title: Container(
             width: double.infinity,
@@ -198,7 +271,7 @@ class _RandomUserListState extends State<RandomUserList> {
                     height: 60,
                     decoration: BoxDecoration(
                         color: Colors.lightBlue,
-                        image: DecorationImage(image: NetworkImage(thumbnail))),
+                        image: DecorationImage(image: NetworkImage(picture))),
                   ),
                 ),
                 SizedBox(
@@ -231,7 +304,7 @@ class _RandomUserListState extends State<RandomUserList> {
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
-                    )
+                    ),
                   ],
                 )
               ],
@@ -244,7 +317,8 @@ class _RandomUserListState extends State<RandomUserList> {
    * Name : openRandomUserInformationDialog
    * <br> Purpose : This method is will open dialog on listview click for showing information of the user.
    */
-  openRandomUserInformationDialog(item) {
+  openRandomUserInformationDialog(
+      String picture, String name, String email, String phone, String city) {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -267,9 +341,7 @@ class _RandomUserListState extends State<RandomUserList> {
                         color: Colors.lightBlue,
                         shape: BoxShape.circle,
                         image: DecorationImage(
-                            fit: BoxFit.contain,
-                            image: NetworkImage(
-                                randomUserList[item].picture.large))),
+                            fit: BoxFit.contain, image: NetworkImage(picture))),
                   ),
                   SizedBox(
                     height: 5.0,
@@ -279,7 +351,7 @@ class _RandomUserListState extends State<RandomUserList> {
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
                       Text(
-                        '${randomUserList[item].name.first + " " + randomUserList[item].name.last}',
+                        '${name}',
                         style: TextStyle(
                             fontSize: 24.0, fontWeight: FontWeight.bold),
                       ),
@@ -298,7 +370,7 @@ class _RandomUserListState extends State<RandomUserList> {
                     child: Column(
                       children: [
                         Text(
-                          'E-Mail: ${randomUserList[item].email}',
+                          'E-Mail: ${email}',
                           textAlign: TextAlign.start,
                           style: TextStyle(fontSize: 15.0),
                         ),
@@ -306,7 +378,7 @@ class _RandomUserListState extends State<RandomUserList> {
                           height: 5.0,
                         ),
                         Text(
-                          'Phone: ${randomUserList[item].phone}',
+                          'Phone: ${phone}',
                           textAlign: TextAlign.start,
                           style: TextStyle(fontSize: 15.0),
                         ),
@@ -314,7 +386,7 @@ class _RandomUserListState extends State<RandomUserList> {
                           height: 5.0,
                         ),
                         Text(
-                          'City: ${randomUserList[item].location.city}',
+                          'City: ${city}',
                           textAlign: TextAlign.start,
                           style: TextStyle(fontSize: 15.0),
                         ),
@@ -343,17 +415,33 @@ class _RandomUserListState extends State<RandomUserList> {
     }
 
     randomUserList.forEach((randomUser) {
-      if (randomUser.name.first.toLowerCase().contains(text) ||
-          randomUser.name.last.toLowerCase().contains(text))
+      if (randomUser.name.first.toLowerCase().trim().contains(text) ||
+          randomUser.name.last.toLowerCase().trim().contains(text))
         searchResultList.add(randomUser);
     });
     setState(() {
-      debugPrint('SIZE:>>>>>>> ' + searchResultList.length.toString());
       if (searchResultList.length == 0) {
         noMatchFound = true;
       } else {
         noMatchFound = false;
       }
     });
+  }
+
+  /**
+   * Name : showProgressBar
+   * <br> Purpose : This method is for showing progressDialog.
+   */
+  showProgressDialog() {
+    progressdialog = new ProgressDialog(context);
+    progressdialog.style(
+      message: 'Adding data.....',
+      progressWidget: Transform.scale(
+        scale: 0.5,
+        child: CircularProgressIndicator(),
+      ),
+      insetAnimCurve: Curves.easeInOut,
+    );
+    progressdialog.show();
   }
 }
